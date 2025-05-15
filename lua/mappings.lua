c3642081112dc22c4282c6ea6119a444ec379078
Coffee Bean <beanc904@outlook.com>
2025-05-15 22:49:28 +0800

    add keymap of v-JK

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,8 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
+map("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move selected line down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move selected line up" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
@@ -17,7 +17,7 @@ map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
 -- <leader>rn 的重新绑定
 vim.keymap.del("n", "<leader>rn")
 map("n", "<leader>rN", function()
-  vim.opt.relativenumber = not vim.opt.relativenumber
+  vim.opt.relativenumber = not vim.opt.relativenumber:get()
 end, { silent = true, noremap = true, desc = "Toggle Relative Number" })
 map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
