a32d9a218278e00906b72340d62a9915d1332475
Coffee Bean <beanc904@gmail.com>
2025-11-01 20:33:28 +0800

    optimize the meson snippets

--- lua/snippets/meson.lua
+++ lua/snippets/meson.lua
@@ -7,7 +7,7 @@ local rep = require("luasnip.extras").rep
 
 ls.add_snippets("meson", {
   s(
-    "ccproj",
+    "ccexe",
     fmt(
       [[
 project(
@@ -19,9 +19,13 @@ project(
   subproject_dir  : {subproject_dir},
 )
 
+{sub1} = subproject('{sub2}')
+{sub3}_dep = {sub4}.get_variable('{lib_dep}')
+
 executable(
   '{exe_name}',
   [{sources}],
+  dependencies : [{sub5}_dep],
   install : true
 )
       ]],
@@ -35,8 +39,14 @@ executable(
           "'cpp_std=c++17', 'warning_level=3', 'optimization=2'"
         ),
         subproject_dir = i(6, "'subprojects'"),
+        sub1 = i(7, "sub"),
+        sub2 = rep(7),
+        sub3 = rep(7),
+        sub4 = rep(7),
+        sub5 = rep(7),
+        lib_dep = i(8, "lib_dep"),
         exe_name = rep(1),
-        sources = i(7, "'main.cc'"),
+        sources = i(9, "'main.cc'"),
       }
     )
   ),
@@ -53,8 +63,8 @@ project(
   default_options : [{default_options}],
 )
 
-library(
-  '{lib_name}',
+{lib1} = library(
+  '{lib2}',
   [{sources}],
   include_directories : include_directories('.'),
   install : true
@@ -62,11 +72,14 @@ library(
 
 lib_dep = declare_dependency(
   include_directories : include_directories('.'),
-  link_with : {lib_name}
+  link_with : {lib3}
 )
       ]],
       {
         lib_name = i(1, "my_lib"),
+        lib1 = rep(1),
+        lib2 = rep(1),
+        lib3 = rep(1),
         languages = i(2, "cpp"),
         version = i(3, "'0.1.0'"),
         meson_version = i(4, "'>=0.60.0'"),
