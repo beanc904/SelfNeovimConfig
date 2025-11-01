3427afb27b1bec24eceb9442792290ab1e3ea71b
Coffee Bean <beanc904@gmail.com>
2025-11-01 17:39:40 +0800

    add meson init snippets and buildtools quick keymap

--- lua/snippets/meson.lua
+++ lua/snippets/meson.lua
@@ -1,22 +1,78 @@
-local ls = require("luasnip")
+local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 
-return {
-  s("mesoncc", fmt([[
-project('{name}', 'cpp',
-  version : '{version}',
-  default_options : ['cpp_std={std}']
+ls.add_snippets("meson", {
+  s(
+    "ccproj",
+    fmt(
+      [[
+project(
+  '{project_name}',
+  '{languages}',
+  version         : {version},
+  meson_version   : {meson_version},
+  default_options : [{default_options}],
+  subproject_dir  : {subproject_dir},
 )
 
-executable('{target}', ['{src}'])
-  ]], {
-    name = i(1, "my_project"),
-    version = i(2, "0.1.0"),
-    std = i(3, "c++17"),
-    target = i(4, "main"),
-    src = i(5, "main.cc"),
-  })),
-}
+executable(
+  '{exe_name}',
+  [{sources}],
+  install : true
+)
+      ]],
+      {
+        project_name = i(1, "my_project"),
+        languages = i(2, "cpp"),
+        version = i(3, "'0.1.0'"),
+        meson_version = i(4, "'>=0.60.0'"),
+        default_options = i(
+          5,
+          "'cpp_std=c++17', 'warning_level=3', 'optimization=2'"
+        ),
+        subproject_dir = i(6, "'subprojects'"),
+        exe_name = rep(1),
+        sources = i(7, "'main.cc'"),
+      }
+    )
+  ),
+
+  s(
+    "cclib",
+    fmt(
+      [[
+project(
+  '{lib_name}',
+  '{languages}',
+  version         : {version},
+  meson_version   : {meson_version},
+  default_options : [{default_options}],
+)
+
+library(
+  '{lib_name}',
+  [{sources}],
+  include_directories : include_directories('.'),
+  install : true
+)
+
+lib_dep = declare_dependency(
+  include_directories : include_directories('.'),
+  link_with : {lib_name}
+)
+      ]],
+      {
+        lib_name = i(1, "my_lib"),
+        languages = i(2, "cpp"),
+        version = i(3, "'0.1.0'"),
+        meson_version = i(4, "'>=0.60.0'"),
+        default_options = i(5, "'cpp_std=c++17', 'warning_level=3'"),
+        sources = i(6, "'src/foo.cc', 'src/bar.cc'"),
+      }
+    )
+  ),
+})
--- lua/whichkey.lua
+++ lua/whichkey.lua
@@ -35,51 +35,139 @@ wk.add {
   -- some compile commands
   {
     "<leader>ms",
-    "<cmd>!meson setup build<CR>",
+    function()
+      local builddir =
+        vim.fn.input("Meson build directory: ", "builddir/", "dir")
+      local cmd = { "meson", "setup", "--buildtype=debug", builddir }
+      local output = vim.system(cmd):wait()
+      if output.code == 0 then
+        vim.notify("Meson setup successfully.", vim.log.levels.INFO)
+      else
+        vim.notify(
+          "Meson setup failed!\nstderr:\n"
+            .. output.stderr
+            .. "\nstdout:\n"
+            .. output.stdout,
+          vim.log.levels.ERROR
+        )
+      end
+    end,
     icon = { icon = " ", color = "yellow" },
     desc = "Meson setup",
     mode = "n",
   },
   {
     "<leader>mr",
-    "<cmd>!meson setup --reconfigure build<CR>",
+    function()
+      local builddir =
+        vim.fn.input("Meson reconfigure directory: ", "builddir/", "dir")
+      local cmd = { "meson", "setup", "--reconfigure", builddir }
+      local output = vim.system(cmd):wait()
+      if output.code == 0 then
+        vim.notify("Meson reconfigure successfully.", vim.log.levels.INFO)
+      else
+        vim.notify(
+          "Meson reconfigure failed\nstderr:\n"
+            .. output.stderr
+            .. "\nstdout\n"
+            .. output.stdout,
+          vim.log.levels.ERROR
+        )
+      end
+    end,
     icon = { icon = " ", color = "yellow" },
     desc = "Meson reconfigure",
     mode = "n",
   },
   {
     "<leader>mm",
-    "<cmd>!meson compile -C build -j$(nproc)<CR>",
+    function()
+      local builddir =
+        vim.fn.input("Meson compile directory: ", "builddir/", "dir")
+      local cmd = { "meson", "compile", "-C", builddir }
+      local output = vim.system(cmd):wait()
+      if output.code == 0 then
+        vim.notify("Meson compile successfully.", vim.log.levels.INFO)
+      else
+        vim.notify(
+          "Meson compile failed\nstderr:\n"
+            .. output.stderr
+            .. "\nstdout\n"
+            .. output.stdout,
+          vim.log.levels.ERROR
+        )
+      end
+    end,
     icon = { icon = " ", color = "yellow" },
     desc = "Meson compile",
     mode = "n",
   },
   {
-    "<leader>mc",
-    "<cmd>!rm -rf build<CR>",
-    icon = { icon = " ", color = "red" },
-    desc = "Build clean",
-    mode = "n",
-  },
-  {
     "<leader>ml",
-    "<cmd>!ln -sf ./build/compile_commands.json .<CR>",
+    function()
+      local builddir =
+        vim.fn.input("Linked compile_commands.json dir: ", "build/", "dir")
+      local cmd =
+        { "ln", "-sf", "./" .. builddir .. "/compile_commands.json", "." }
+      vim.system(cmd):wait()
+      vim.notify(
+        "Linked compile_commands.json from " .. builddir,
+        vim.log.levels.INFO
+      )
+    end,
     icon = { icon = " ", color = "orange" },
     desc = "Link compile_commands.json",
     mode = "n",
   },
   {
     "<leader>mg",
-    "<cmd>!cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug<CR>",
+    function()
+      local builddir = vim.fn.input("CMake build directory: ", "build/", "dir")
+      local cmd = {
+        "cmake",
+        "-B",
+        builddir,
+        "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
+        "-DCMAKE_BUILD_TYPE=Debug",
+      }
+      local output = vim.system(cmd):wait()
+      if output.code == 0 then
+        vim.notify("CMake generate successfully.", vim.log.levels.INFO)
+      else
+        vim.notify(
+          "CMake generate failed\nstderr:\n"
+            .. output.stderr
+            .. "\nstdout\n"
+            .. output.stdout,
+          vim.log.levels.ERROR
+        )
+      end
+    end,
     icon = { icon = " ", color = "purple" },
     desc = "CMake generate",
     mode = "n",
   },
   {
     "<leader>mb",
-    "<cmd>!cmake --build build -- -j$(nproc)<CR>",
+    function()
+      local builddir =
+        vim.fn.input("CMake compile directory: ", "build/", "dir")
+      local cmd = { "cmake", "--build", builddir }
+      local output = vim.system(cmd):wait()
+      if output.code == 0 then
+        vim.notify("CMake compile successfully.", vim.log.levels.INFO)
+      else
+        vim.notify(
+          "CMake compile failed\nstderr:\n"
+            .. output.stderr
+            .. "\nstdout\n"
+            .. output.stdout,
+          vim.log.levels.ERROR
+        )
+      end
+    end,
     icon = { icon = " ", color = "purple" },
-    desc = "CMake build",
+    desc = "CMake compile",
     mode = "n",
   },
 }
